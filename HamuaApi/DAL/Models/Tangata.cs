c12f0ec9119dac3627513c611a780fd6abb84473
Gavin Downes <Gavin.Downes@twoa.ac.nz>
2021-05-08 12:59:22 +1200

    update tangata

--- HamuaApi/DAL/Models/Tangata.cs
+++ HamuaApi/DAL/Models/Tangata.cs
@@ -12,6 +12,25 @@ namespace HamuaRegistrationApi.DAL.Models
 
         public string LastName { get; set; }
 
+        public string DOB { get; set; }
+        public string PlaceOfBirth { get; set; }
+        public string Occupation { get; set; }
+        public string SpecialtySkills { get; set; }
+
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+        public string HomePhone { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public bool IsTeReoFirstLanguage { get; set; }
+        public bool CanYouSpeakTeReo { get; set; }
+        public string TeReoProficiency { get; set; }
+        public bool ReturnToRuatokiToLive { get; set; }
+        public string ReturnComment { get; set; }
+
         public int ParentId { get; set; }
 
         public ICollection<Marae> NgaMarae { get; set; } = new List<Marae>();
